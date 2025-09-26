59a38032c3d634b0de5a8c6ec12fd3c8e566887d
jloparra <54353180+jloparra@users.noreply.github.com>
2025-09-26 16:25:04 +0200

    Delete types.ts

--- types.ts
+++
@@ -1,7 +0,0 @@
-export type Language = 'es' | 'en' | 'fr';
-
-export interface LanguageContextType {
-  language: Language;
-  setLanguage: (language: Language) => void;
-  t: (key: string, section?: string) => string;
-}
